d248a579e5c38d5b4449a8e9ebbab541d2859bee
phmaa <phmaa@yahoo.com>
2021-03-25 17:27:21 -0400

    test image file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,7 +36,7 @@ const sections = [
       title:'Data Integration',
       para:'Built packages of automated workflows through ETL process and piped data off to a wide range of file types and destinations.',
       link: '#'},
-      {image:'/images/package-350.jpg',
+      {image:'/images/report-350.jpg',
       title:'Data Reporting',
       para:'Created enhanced SSRS reports for analytical purposes to make informed business decisions.',
       link: 'https://phmaa.github.io/reporting/'}]
