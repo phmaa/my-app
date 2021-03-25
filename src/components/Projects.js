7bf474fc4b2b7cd2cc635c864c94ee56878ed196
phmaa <phmaa@yahoo.com>
2021-03-25 19:59:22 -0400

    report.jpg

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -24,7 +24,7 @@ const sections = [
   {
       title:'Databases',
       id:'database',
-      cards:[{image:'/images/temp-350.jpg',
+      cards:[{image:'/images/mysql-350.jpg',
       title:'Transactional Databases',
       para:'Querying and manipulating data from various RDBMS, including Microsoft SQL Server, Oracle, SQLite, MariaDB, PostgreSQL and MySQL.',
       link: 'https://phmaa.github.io/scripts'},
@@ -36,7 +36,7 @@ const sections = [
       title:'Data Integration',
       para:'Built packages of automated workflows through ETL process and piped data off to a wide range of file types and destinations.',
       link: '#'},
-      {image:'/images/mysql-350.jpg',
+      {image:'/images/report-350.jpg',
       title:'Data Reporting',
       para:'Created enhanced SSRS reports for analytical purposes to make informed business decisions.',
       link: 'https://phmaa.github.io/reporting/'}]
