8259ddcf2de67c3a92b39edda8038b0ba9e659f3
phmaa <phmaa@yahoo.com>
2021-05-12 17:48:14 -0400

    modify welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,7 @@ function Welcome() {
   return (
     <section id="welcome">
     <h1><code><span class="keyword">def </span><span class="declaration">greeting</span>():<br/>&nbsp;&nbsp;&nbsp;&nbsp;
-      <span class="keyword">return </span>"Hi, I'm Hwey"</code>
+      <span class="keyword">return </span>"Hi, I'm Hwey."</code>
     </h1>
     <div class="typewriter">
       <p>"You never fail until you stop trying."</p>
