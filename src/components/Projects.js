34566072d71c2af8bc1085f74e1f06f277db47da
Hwey <phmaa@yahoo.com>
2022-02-26 16:14:37 -0500

    add link to container image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,26 +2,6 @@ import Section from './Sections';
 
 const sections = [
   {
-      title:'Web Development',
-      id:'webdev',
-      cards:[{image:'images/tea-350.jpg',
-              title:'Bootstrap 5',
-              para:'A fully responsive proof of concept page built with Bootstrap 5.',
-              link: 'https://phmaa.github.io/bootstrap5/'},
-              {image:'images/flex-350.jpg',
-              title:'Flexbox',
-              para:'A CSS Flexbox model that automatically arranges elements based on the viewport sizes.',
-              link: 'https://phmaa.github.io/project-flexbox/'},
-              {image:'images/silhouet-350.jpg',
-              title:'HD Images',
-              para:'Provide the optimal image file to the browser based on device pixel ratio and image size.',
-              link: 'https://phmaa.github.io/projectHD/'},
-              {image:'images/animation-350.jpg',
-              title:'Animations',
-              para:'Static webpage with a focus on tranforms, transitions, and animations.',
-              link: 'https://phmaa.github.io/animations/'}]
-},
-  {
       title:'Data Analytics',
       id:'database',
       cards:[      {image:'images/package-350.jpg',
@@ -47,9 +27,9 @@ const sections = [
     para:'Classical cipher decryption using frequency analysis as well as other techinques like Kasiski test and the index of coincidence.',
     link: 'https://phmaa.github.io/crypto'},
     {image:'images/vampire-350.jpg',
-    title:'JavaScript',
-    para:'A team project that interacts with users and applies Google Visualziation API to render results.',
-    link: 'https://phmaa.github.io/vampire'},
+    title:'Docker Images',
+    para:'Developed Python Flask apps using Jinja2 templates. Deployed dockerized images to Google Cloud using Cloud Run',
+    link: 'https://inspire-ylbrla4qna-uc.a.run.app'},
     {image:'images/java-350.jpg',
     title:'Java',
     para:'Dijkstra\'s shortest paths alogrithm and Kosaraju\'s algorithm for finding strongly connected compoents of a directed graph.',
@@ -57,6 +37,26 @@ const sections = [
     {image:'images/android-350.jpg',
     title:'Android',
     para:'Applied OOP techniques to build and unit-test apps featuring various components including activities, services, and content providers.'}]
+},
+{
+  title:'Web Development',
+  id:'webdev',
+  cards:[{image:'images/tea-350.jpg',
+          title:'Bootstrap 5',
+          para:'A fully responsive proof of concept page built with Bootstrap 5.',
+          link: 'https://phmaa.github.io/bootstrap5/'},
+          {image:'images/flex-350.jpg',
+          title:'Flexbox',
+          para:'A CSS Flexbox model that automatically arranges elements based on the viewport sizes.',
+          link: 'https://phmaa.github.io/project-flexbox/'},
+          {image:'images/silhouet-350.jpg',
+          title:'HD Images',
+          para:'Provide the optimal image file to the browser based on device pixel ratio and image size.',
+          link: 'https://phmaa.github.io/projectHD/'},
+          {image:'images/animation-350.jpg',
+          title:'Animations',
+          para:'Static webpage with a focus on tranforms, transitions, and animations.',
+          link: 'https://phmaa.github.io/animations/'}]
 }];
 
 function Project() {
